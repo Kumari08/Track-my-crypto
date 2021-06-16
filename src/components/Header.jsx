b2c92b011a95e039b59badc8f87f355b562e79a8
Kumari Neha <71456072+Kumari08@users.noreply.github.com>
2021-06-16 22:54:34 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1 className="text-center text-warning mt-3 mb-4">Coinstar</h1>
+      <h1 className="text-center text-warning mt-3 mb-4">Track-my-Crypto</h1>
     </div>
   );
 };
